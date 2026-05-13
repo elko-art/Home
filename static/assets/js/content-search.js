27468ec2093970cac9eccf9b7e4adf3a2c850162
Elko-Utopia <elkoutopia@gmail.com>
2026-05-13 12:04:45 -0400

    update

--- static/assets/js/content-search.js
+++ static/assets/js/content-search.js
@@ -1,90 +1,136 @@
-//关键词sug
-var hotList = 0;
+// 动态搜索引擎联想推荐 - 根据当前选中的搜索引擎切换联想来源
+var currentSearchEngine = 'google';
+
 $(function() {
-    //当键盘键被松开时发送Ajax获取数据
-    $('#search-text').keyup(function() {
+    // 初始化：读取当前选中的搜索引擎
+    updateSearchEngine();
+
+    // 监听搜索引擎选择变化
+    $(document).on('change', 'input[name="type2"]', function() {
+        updateSearchEngine();
+    });
+
+    // 更新当前搜索引擎
+    function updateSearchEngine() {
+        var selectedId = $('input[name="type2"]:checked').attr('id') || 'm_type-google1';
+        if (selectedId.includes('google')) {
+            currentSearchEngine = 'google';
+        } else if (selectedId.includes('bing')) {
+            currentSearchEngine = 'bing';
+        } else if (selectedId.includes('duckduckgo')) {
+            currentSearchEngine = 'duckduckgo';
+        } else {
+            currentSearchEngine = 'none';
+        }
+    }
+
+    // 监听搜索框输入，根据当前引擎请求联想
+    $(document).on('input', '#m_search-text', function() {
         var keywords = $(this).val();
-        if (keywords == '') { $('#word').hide(); return };
+        if (keywords == '' || keywords.length < 1) { 
+            $('#word').empty().hide();
+            return;
+        }
+        
+        if (currentSearchEngine === 'google') {
+            requestGoogleSuggestions(keywords);
+        } else if (currentSearchEngine === 'bing') {
+            requestBingSuggestions(keywords);
+        } else if (currentSearchEngine === 'duckduckgo') {
+            requestDuckDuckGoSuggestions(keywords);
+        } else {
+            $('#word').empty().hide();
+        }
+    });
+
+    // Google 联想
+    function requestGoogleSuggestions(keywords) {
         $.ajax({
-            url: 'https://suggestion.baidu.com/su?wd=' + keywords,
+            url: 'https://www.google.com/complete/search?client=chrome&q=' + encodeURIComponent(keywords),
             dataType: 'jsonp',
-            jsonp: 'cb', //回调函数的参数名(键值)key
-            // jsonpCallback: 'fun', //回调函数名(值) value
-            beforeSend: function() {
-                // $('#word').append('<li>正在加载。。。</li>');
-            },
+            jsonp: 'callback',
+            timeout: 3000,
             success: function(res) {
-                $('#word').empty().show();
-                hotList = res.s.length;
-                if (hotList) {
-                    $("#word").css("display", "block");
-                    for (var i = 0; i < hotList-1; i++) {
-                        if (i===hotList-1){
-                            $("#word").append('<li id="lastHot"><span>' + (i + 1) + "</span>" + res.s[i] + "</li>");
-                        }
-                        else{
-                            $("#word").append("<li><span>" + (i + 1) + "</span>" + res.s[i] + "</li>");
-                        }
-                        $("#word li").eq(i).click(function() {
-                            $('#search-text').val(this.childNodes[1].nodeValue);
-                            window.open(thisSearch + this.childNodes[1].nodeValue);
-                            $('#word').css('display', 'none')
-                        });
-                        if (i === 0) {
-                            $("#word ul li").eq(i).css({
-                                "border-top": "none"
-                            });
-                            $("#word ul span").eq(i).css({
-                                "color": "#fff",
-                                "background": "#f54545"
-                            })
-                        } else if (i === 1) {
-                            $("#word ul span").eq(i).css({
-                                "color": "#fff",
-                                "background": "#ff8547"
-                            })
-                        } else if (i === 2) {
-                            $("#word ul span").eq(i).css({
-                                "color": "#fff",
-                                "background": "#ffac38"
-                            })
-                        }
-                    } 
+                if (res && res[1]) {
+                    displaySuggestions(res[1].slice(0, 8));
                 } else {
-                        $("#word").css("display", "none")
+                    $('#word').empty().hide();
                 }
-                    
-                /*
-                if (data.s == '') {
-                    //$('#word').append('<div class="error">Not find  "' + keywords + '"</div>');
-                    $('#word').empty();
-                    $('#word').hide();
-                }
-                $.each(data.s, function() {
-                    $('#word').append('<li>' + this + '</li>');
-                })*/
             },
             error: function() {
-                $('#word').empty().show();
-                //$('#word').append('<div class="click_work">Fail "' + keywords + '"</div>');
-                $('#word').hide();
+                $('#word').empty().hide();
             }
-        })
-    })
+        });
+    }
 
-    //点击搜索数据复制给搜索框
+    // Bing 联想
+    function requestBingSuggestions(keywords) {
+        $.ajax({
+            url: 'https://www.bing.com/osjson.aspx?query=' + encodeURIComponent(keywords),
+            dataType: 'json',
+            timeout: 3000,
+            success: function(res) {
+                if (res && res[1]) {
+                    displaySuggestions(res[1].slice(0, 8));
+                } else {
+                    $('#word').empty().hide();
+                }
+            },
+            error: function() {
+                $('#word').empty().hide();
+            }
+        });
+    }
+
+    // DuckDuckGo 联想
+    function requestDuckDuckGoSuggestions(keywords) {
+        $.ajax({
+            url: 'https://duckduckgo.com/ac?q=' + encodeURIComponent(keywords),
+            dataType: 'json',
+            timeout: 3000,
+            success: function(res) {
+                if (res && res.length > 0) {
+                    var suggestions = res.slice(0, 8).map(function(item) {
+                        return item.phrase || '';
+                    }).filter(function(item) { return item; });
+                    displaySuggestions(suggestions);
+                } else {
+                    $('#word').empty().hide();
+                }
+            },
+            error: function() {
+                $('#word').empty().hide();
+            }
+        });
+    }
+
+    // 显示联想列表
+    function displaySuggestions(suggestions) {
+        if (!suggestions || suggestions.length === 0) {
+            $('#word').empty().hide();
+            return;
+        }
+        
+        $('#word').empty().show();
+        $.each(suggestions, function(i, suggestion) {
+            var rankColor = ['#f54545', '#ff8547', '#ffac38'];
+            var color = rankColor[i] || '#999';
+            var html = '<li><span style="color: #fff; background: ' + color + ';">' + (i + 1) + '</span>' + suggestion + '</li>';
+            $('#word').append(html);
+        });
+        $('#word').css('display', 'block');
+    }
+
+    // 点击联想项
     $(document).on('click', '#word li', function() {
-        var word = $(this).text().replace(/^[0-9]/, '');
-        $('#search-text').val(word);
-        $('#word').empty();
-        $('#word').hide();
-        //$("form").submit();
-         $('.submit').trigger('click');//触发搜索事件
-    })
-    //$(document).on('click', '.container,.banner-video,nav', function() {
-    $(document).on('click', '.io-grey-mode', function() {
-        $('#word').empty();
-        $('#word').hide();
-    })
+        var word = $(this).text().replace(/^[0-9]/, '').trim();
+        $('#m_search-text').val(word);
+        $('#word').empty().hide();
+        $('.super-search-fm').submit();
+    });
 
+    // 点击其他地方隐藏联想
+    $(document).on('click', '.io-grey-mode, .panel-body', function() {
+        $('#word').empty().hide();
+    });
 })
